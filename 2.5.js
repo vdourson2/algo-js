13e1d09fc692878b18da37348c891e451589549f
Virginie Dourson <virginiedourson@gmail.com>
2023-09-08 14:37:42 +0200

    Exercises 2.5 and 2.6

---
+++ 2.5.js
@@ -0,0 +1,5 @@
+let number = prompt("What's your favorite number?", "Number");
+while (number != 42){
+    number = prompt("Are you sure? What's your favorite number");
+}
+alert("Finally!");
---
+++ 2.6.js
@@ -0,0 +1,24 @@
+let number = Number(prompt("Give a number between 1 and 7", "Number"));
+switch (number) {
+    case 1:
+        alert("Monday");
+        break;
+    case 2:
+        alert("Tuesday");
+        break;
+    case 3:
+        alert("Wednesday");
+        break;
+    case 4:
+        alert("Tursday");
+        break;
+    case 5:
+        alert("Friday");
+        break;
+    case 6:
+        alert("Saturday");
+        break;
+    case 7:
+        alert("Sunday");
+        break;
+}
