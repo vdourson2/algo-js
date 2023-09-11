c9bdfbb310451f984e9cb87a5a108ef35e12c744
Virginie Dourson <virginiedourson@gmail.com>
2023-09-11 11:07:21 +0200

    Exercise 4.3

---
+++ 4.3.js
@@ -0,0 +1,16 @@
+//Function that returns a random integer between 1 and 10
+function rand10() {
+    return Math.floor(Math.random()*10);
+}
+
+//function that returns an array of n numbers between 1 and 10
+let multiRand = (n) => {
+    let arrayRandomNumbers = [];
+    for (let i=0; i<n; i++) {
+        arrayRandomNumbers.push(rand10());
+    } 
+    return arrayRandomNumbers;
+}
+
+let num = Number(prompt("How many numbers do you need?"));
+alert(multiRand(num));
