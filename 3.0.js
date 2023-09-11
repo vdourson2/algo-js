a9bae4c715c8a6ab46b3e6074042090354b46f78
Virginie Dourson <virginiedourson@gmail.com>
2023-09-11 09:25:56 +0200

    Exercise 3.0

---
+++ 3.0.js
@@ -0,0 +1,29 @@
+let students =[
+        "Elodie", 
+        "Mathias", 
+        "Lucas", 
+        "Rosalie", 
+        "Lidwine", 
+        "Stephane", 
+        "Tim", 
+        "Virginie", 
+        "Justine", 
+        "Carole", 
+        "Thomas", 
+        "Antoine", 
+        "Kimi", 
+        "Valentin", 
+        "Justine", 
+        "Elisabeth", 
+        "Pierre", 
+        "Justin", 
+        "Robin", 
+        "Layla", 
+        "Marie", 
+        "Alexandre Vdw", 
+        "Alexandre V", 
+        "Bastien", 
+        "Dorian"];
+for (let student of students) {
+    console.log(student);
+}
