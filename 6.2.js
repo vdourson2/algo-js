bf64179bca1e17f3c83db59d893db284ccddd453
Virginie Dourson <virginiedourson@gmail.com>
2023-09-12 17:11:50 +0200

    Exercise 6.2 and README

---
+++ 6.2.js
@@ -0,0 +1,38 @@
+// Create a class name Rectangle with attributes topLeftXPos, topLeftYPos, width and length. 
+// Add a method named collides(otherRectangle) that returns true only if the current rectangle collides with otherRectangle.
+
+// Test the collides(otherRectangle) method by using multiple test cases.
+
+class Rectangle {
+    constructor(topLeftXPos, topLeftYPos, width, length){
+        this.topLeftXPos = topLeftXPos;
+        this.topLeftYPos = topLeftYPos;
+        this.width = width;
+        this.length = length;
+    }
+
+    collides(otherRectangle){
+        return !((otherRectangle.topLeftYPos > (this.topLeftYPos+this.length))||
+        ((otherRectangle.topLeftYPos + otherRectangle.length) < (this.topLeftYPos)) ||
+        (otherRectangle.topLeftXPos > (this.topLeftXPos+this.width))||
+        ((otherRectangle.topLeftXPos + otherRectangle.width) < (this.topLeftXPos)))
+    }
+}
+
+let greenRect = new Rectangle(3,6,4,3);
+let redRect = new Rectangle(8,5,2,5);
+let purpleRect = new Rectangle(0,9,2,1);
+let blueRect = new Rectangle(1,5,3,2);
+let yellowRect = new Rectangle(4,8,3,2);
+
+console.log("Green collides red?");
+console.log(greenRect.collides(redRect));
+
+console.log("Green collides purple?");
+console.log(greenRect.collides(purpleRect));
+
+console.log("Green collides blue?");
+console.log(greenRect.collides(blueRect));
+
+console.log("Green collides yellow?");
+console.log(greenRect.collides(yellowRect));
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # algo-js
-Becode exercise : learn the basics of algorithmics with JavaScript
+Becode exercise : learn the basics of algorithmics with JavaScript : control structures, arrays, functions, objects and classes.
+
+## Briefing
+[Instructions](https://github.com/becodeorg/CRL-KELLER-6/tree/main/1.TRAIL/1.The-Field/5.Js) for the exercise.
+
+## Extras
+Figure of rectangles used to test exercise 6.2 :
+![Schema rectangle](SchemaRectanglesEx6_2.png)
+
+## Used languages
+Javascript
+
+## Authors
+Virginie Dourson
+
+## Date
+09/2023
+
+## Progression
+WIP
+
