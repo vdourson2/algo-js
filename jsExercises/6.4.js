a24e6414d25c3774d1f1e597fcae2d42eb717611
Virginie Dourson <virginiedourson@gmail.com>
2023-09-13 14:28:20 +0200

    Finishes Exercise 6.4 (extra)

--- jsExercises/6.4.js
+++ jsExercises/6.4.js
@@ -28,7 +28,7 @@ function randomN(size){
 function generateRect(n) {
     let rectangles = [];
     for (let i=0; i<n; i++) {
-        let r = new Rectangle(randomN(200),randomN(200),randomN(50)+50,randomN(50)+50);
+        let r = new Rectangle(randomN(300),randomN(300),randomN(50)+50,randomN(50)+50);
         rectangles.push(r);
     }
     return rectangles;
@@ -37,27 +37,27 @@ function generateRect(n) {
 //This function takes an array of objects of the class "Rectangle" 
 //and displays all colliding rectangles in this array .
 function collidings(arrayRect) {
-    arrayRect.map((r,index) => r.index = index);
-    for (var ind in arrayRect) {
+    let ul = document.querySelector(".list");
+    arrayRect.map((r,index) => r.index = index); //Add index of the element in the objet keep it after filter
+    for (var ind in arrayRectangles) {
         if (arrayRect.length > 1) {
-            let rect1 = arrayRect.splice(0,1);
-            let collidesWith = arrayRect.filter((nextRectangle) => rect1[0].collides(nextRectangle));
-            console.log("Rectangle " + ind + " collides with : " );
-            collidesWith.forEach((el) => console.log(el.index));
-            console.log(collidesWith);
+            let rect1 = arrayRect.splice(0,1);//Suppress first element of the array
+            let collidesWith = arrayRect.filter((nextRectangle) => rect1[0].collides(nextRectangle));//Keep only rectangles that collide with the first one
+            collidesWith.forEach((el, i) => {
+                ul.appendChild(document.createElement("li")).textContent = `Rectangle ${rect1[0].index} collides with rectangle ${collidesWith[i].index}`;
+            });
         }
     }
 }
 
 //Generate an array of rectangles
-let arrayRectangles = generateRect(3);
+let arrayRectangles = generateRect(10);
 console.log("The array with rectangles is : ");
 console.table(arrayRectangles);
 
 //Search and display colliding rectangles
 let arrayToSort = [...arrayRectangles];
 collidings(arrayToSort);
-
 
 //Display rectangles in canvas
 const canvas = document.querySelector("canvas");
@@ -67,11 +67,9 @@ const ctx = canvas.getContext("2d");
     for (let el of arrayRectangles) {
         ctx.fillStyle = `rgba(${randomN(256)},${randomN(256)},${randomN(256)},0.5)`;
         ctx.fillRect(el.topLeftXPos,el.topLeftYPos,el.width,el.length);
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center"; 
+        ctx.fillText(`${el.index}`,(el.topLeftXPos + el.width/2),(el.topLeftYPos+(el.length+20)/2));
     }
 })();
-
-//This function takes as argument an integer "size",
-// and generate a random integer between 0 and "size".
-function randomN(size){
-    return Math.floor(Math.random()*size);
-}
